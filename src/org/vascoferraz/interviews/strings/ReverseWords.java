063e396a41e9c53a991db86b0f9b31a75715b858
vascoferraz <vascoferraz@hotmail.com>
2021-05-01 21:23:13 +0100

    add ReverseWords training exercise

--- src/org/vascoferraz/interviews/strings/ReverseWords.java
+++ src/org/vascoferraz/interviews/strings/ReverseWords.java
@@ -1,2 +1,22 @@
-package org.vascoferraz.interviews.strings;public class ReverseWords {
+package org.vascoferraz.interviews.strings;
+
+public class ReverseWords {
+
+    public static void main(String[] args) {
+
+        String message = "Hello, my name is Vasco";
+        String[] splitMessage = message.split(" ");
+        int size = splitMessage.length;
+        String[] reverseMessage = new String[size];
+        int counter = 0;
+
+        for (int i = splitMessage.length; i > 0; i--) {
+            reverseMessage[counter] = splitMessage[i - 1];
+            counter++;
+        }
+
+        for (String str : reverseMessage) {
+            System.out.println(str);
+        }
+    }
 }
