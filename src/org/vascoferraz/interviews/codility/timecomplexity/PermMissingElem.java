c6ac4ad3e7efd9f7e39a11e8c4fb859f39483083
vascoferraz <vascoferraz@hotmail.com>
2021-05-18 15:52:16 +0100

    add PermMissingElem (timecomplexity) Codility exercise

---
+++ src/org/vascoferraz/interviews/codility/timecomplexity/PermMissingElem.java
@@ -0,0 +1,27 @@
+package org.vascoferraz.interviews.codility.timecomplexity;
+
+import java.util.Arrays;
+
+public class PermMissingElem {
+
+    public static void main(String[] args) {
+
+        int[] array = {2, 3, 1, 4, 5, 6};
+        solution(array);
+    }
+
+    public static int solution(int[] A) {
+
+        Arrays.sort(A);
+
+        for (int i = 0; i < A.length; i++) {
+
+            if (A[i] != i + 1) {
+                //System.out.println(i + 1);
+                return i + 1;
+            }
+        }
+        //System.out.println(A.length + 1);
+        return A.length + 1;
+    }
+}
